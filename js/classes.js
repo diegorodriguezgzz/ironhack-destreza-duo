dda4396084b0e6454bc665f91ce6e4a86b3fa801
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2018-11-28 00:59:00 -0600

    Corrected board layout, added basic piece and board methods

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,10 @@
+/* Colors:  
+  Blue (board): #0023B9
+  Yellow (piece): #FFC300
+  Red: #FF0000
+  Gray: #777777
+*/
+
 /* Game pieces catalogue */
 /* Consider allowing color-coded rows for a low-difficulty challenge*/
 let rowA = [
@@ -41,35 +48,35 @@ let rowA = [
 let rowB = [
   {
     id: "1B",
-    description: "Star",
+    description: "Five-Point-Star",
     syms: 5, 
     rots: [0],
     color: "#777777"
   },
   {
     id: "2B",
-    description: "Triskelion",
-    syms: 3,
-    rots: [0],
+    description: "Hotdog",
+    syms: 2,
+    rots: [0,2],
     color: "#777777"
   },
   {
     id: "3B",
-    description: "Diamond",
-    syms: 2,
-    rots: [0,2],
+    description: "Right-triangle",
+    syms: 1,
+    rots: [0],
     color: "#777777"
   },  
   {
     id: "4B",
-    description: "Hexagon",
+    description: "Triskelion",
     syms: 3,
-    rots: [0,2],
+    rots: [0],
     color: "#777777"
   },  
   {
     id: "5B",
-    description: "Hotdog",
+    description: "Rectangle",
     syms: 2,
     rots: [0,2],
     color: "#777777"
@@ -124,7 +131,7 @@ let rowD = [
   },
   {
     id: "2D",
-    description: "Equilateral-triangle",
+    description: "Trapezoid",
     syms: 3,
     rots: [0],
     color: "#777777"
@@ -138,9 +145,9 @@ let rowD = [
   },  
   {
     id: "4D",
-    description: "Right-triangle",
-    syms: 1,
-    rots: [0],
+    description: "Diamond",
+    syms: 2,
+    rots: [0,2],
     color: "#777777"
   },  
   {
@@ -183,7 +190,7 @@ let rowE = [
   },  
   {
     id: "5E",
-    description: "Trapezoid",
+    description: "Equilateral-triangle",
     syms: 3,
     rots: [0],
     color: "#777777"
@@ -204,23 +211,80 @@ function Piece(id, description, syms, rots, color) {
   this.syms = syms;
   this.rots = rots;
   this.color = color;
-};
+  this.position = Math.floor(Math.random()*4); //Out of possible rotations
+  this.x = 0; //For drawing on the board
+  this.y = 0;
+  this.width = 20; //For scaling
+  this.height = 20;
+}
 
 //Functions are defined within prototype to save memory
 Piece.prototype.select = function(player) {
   player.holds = this; //Player holds the piece
   this.color = player.color; //Piece color matches the player's color
-}; //Should I bind something here??
+} //Should I bind something here??
 
-function Player(name, color) {
+Piece.prototype.rotateLeft = function() {
+  this.position = (this.position === 3) ? 0 : this.position + 1;
+}
+
+Piece.prototype.rotateRight = function() {
+  this.position = (this.position === 0) ? 3 : this.position - 1;
+}
+
+//Consider outright using a flat array in first place to improve performance
+//Flatten pieces array for easy shuffling and sampling
+pieces = pieces[0].concat(pieces[1],pieces[2],pieces[3],pieces[4]);
+
+//Bind class to collection of pieces
+pieces = pieces.map(el => new Piece(el.id, el.description, el.syms, el.rots, el.color));
+
+function Board(rows) {
+  this.rows = rows; //3, 4 or 5
+  this.slots = [];
+  this.color = "#0023B9";
+}
+
+Board.prototype.fillBoard = function() {
+  let boardSize = this.rows**2; //Board is always square
+  this.slots = sample(pieces, boardSize);
+}
+
+function Player(name, color, number) {
   this.name = name;
   this.color = color;
-};
+  this.number = number;
+}
 
 function Game(mode) {
   this.mode = mode; //1P Classic, 1P Easy, 2P
-};
+}
 
 function Chrono(time) {
   this.startTime = time;
-};
+}
+
+//Fisher-Yates (FY) Shuffle
+//Special thanks to Mike Bostock at https://bost.ocks.org/mike/shuffle/compare.html
+function shuffle(array) {
+  let m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+}
+
+//Sampling w/o replacement, building on the FY Shuffle
+function sample(array, size) {
+  if (array.length >= size) {
+    newArray = array.slice(0); //First make a shallow copy of the array
+    shuffle(newArray); //Then shuffle the new array
+    newArray = newArray.slice(0, size); //Then only keep indexes up to size
+    return newArray;
+  }
+  else {
+    console.error("Stats error: array size must be larger than sample size");
+  }
+}
