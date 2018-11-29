796f4b46d44b7cbe9cd9cad36e74489fa2cf7742
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2018-11-29 00:42:54 -0600

    With basic mockup for tests and improvements to classes

--- js/classes.js
+++ js/classes.js
@@ -22,6 +22,7 @@ function Piece(id, description, syms, rots, color) {
 }
 
 //Functions are defined within prototype to save memory
+//TODO: Revisit this, it seems like it doesn't make any sense anymore
 Piece.prototype.select = function(player) {
   player.holds = this; //Player holds the piece
   this.color = player.color; //Piece color matches the player's color
@@ -76,28 +77,36 @@ through DOM interactions */
 //Seems to work well
 Player.prototype.grabPiece = function(game, index) {
   this.heldPiece = game.availablePieces[index];
+  selectedPiece(this.heldPiece); //For graphic display
 }
 
-//TO DO: complete
-Player.prototype.placePiece = function(board, game) {
-  if (board.selectedSlot.id === this.heldPiece.id && //The pieces match
+//TODO: complete
+Player.prototype.placePiece = function(game) {
+  if (game.board.selectedSlot.id === this.heldPiece.id && //The pieces match
       this.heldPiece.checkRotation()) { //Accurately rotated
       game.checkedPieces.push(
         game.availablePieces.splice(
           game.availablePieces.indexOf(this.heldPiece), 1
           )[0] //So this returns an element and not a full array
         ) //Just move the one piece from one array to the other
-        
-        //Agregar pieza a piezas acomodadas
+      placedPiece(this.heldPiece, game)//Notify the player
+      game.checkFinished(game.board); //And check if the game is over
+  }
+  else if (!this.heldPiece.checkRotation()) {
+    wrongRot();
   }
   else {
-    //Mostrar mensaje de alerta
-    //Tocar tono de equivocación
-    //Regresar pieza a tablero en posición original
+    wrongTone();
+    wrongPiece();//Mostrar mensaje de alerta
+    updateHighlights();
   }
+  //TODO: Check if this is the best solution
+  this.heldPiece = null; //Regresar pieza a tablero en posición original
+  game.board.selectedSlot = null;
+  this.clickedSlot = null;
 }
 
-//TO DO: complete
+//TODO: complete
 Player.prototype.clickSlot = function(slot) {
   this.clickedSlot = slot;
 }
@@ -108,34 +117,61 @@ function Game(mode, pieces) {
   this.checkedPieces = [];
 }
 
+Game.prototype.buildParameters = function() {
+  //TODO: Extend this with a switch statement
+  this.modeParams = { //Test parameters
+    time : 999, //260
+    players : 1,
+    rows : 3
+  }
+}
+
+Game.prototype.beginGame = function() {
+  this.buildParameters();
+  this.board = new Board(this.modeParams.rows);
+  this.board.fillBoard();
+  this.players = [];
+  for (let i = 1; i <= this.modeParams.players; i++) {
+    this.players.push(createPlayer(i)); //The param is playerNumber 
+  }
+  //Make players, as well
+  this.filterPieces(this.board);
+  this.timer = new Chrono(this.modeParams.time);
+  this.interval = setInterval(update, 1000, this); //¡Funciona!
+  gfxSetup(this);
+}
+
 //Seems to work well
-Game.prototype.filterPieces = function(board) { //Keep only the pieces that are relevant
-  this.availablePieces = this.availablePieces.filter(el => board.slots.indexOf(el) !== -1);
+Game.prototype.filterPieces = function() { //Keep only the pieces that are relevant
+  this.availablePieces = this.availablePieces.filter(el => this.board.slots.indexOf(el) !== -1);
 }
 
-//TO DO: Test
-Game.prototype.checkFinished = function(board) { //Solo
-  return (this.checkedPieces.length === board.slots.length) ? true : false;
+//TODO: Test
+Game.prototype.checkFinished = function() { //Solo
+  return (this.checkedPieces.length === this.board.slots.length) ? true : false;
 }
 
-//TO DO: Test
-Game.prototype.checkGameOver = function(timer) {
-  return (timer.secondsLeft <= 0) ? true : false; //Return true if time is over
+//TODO: Test
+Game.prototype.checkGameOver = function() {
+  return (this.timer.secondsLeft <= 0) ? true : false; //Return true if time is over
 }
 
-//TO DO: Build
+//TODO: Build
 Game.prototype.checkWinner = function() {
   //Check who, out of the available players, did best in the game
 }
 
-//TO DO: Build
+//TODO: Build
 function Chrono(time) {
-  this.startTime = time;
-  this.secondsLeft = 90;
+  // this.startTime = time; Datetime
+  this.secondsLeft = time;
 }
 
 //Function to tick
-//Function to set seconds left
+Chrono.prototype.tickDown = function() { //FIXIT: Tenía game como param
+  this.secondsLeft--;
+}
+
 
 /* Generic utility functions */
 
@@ -154,7 +190,7 @@ function shuffle(array) {
 //Sampling w/o replacement, building on the FY Shuffle
 function sample(array, size) {
   if (array.length >= size) {
-    newArray = array.slice(0); //First make a shallow copy of the array
+    let newArray = array.slice(0); //First make a shallow copy of the array
     shuffle(newArray); //Then shuffle the new array
     newArray = newArray.slice(0, size); //Then only keep indexes up to size
     return newArray;
@@ -164,24 +200,96 @@ function sample(array, size) {
   }
 }
 
-/* Set up game*/
-//Consider outright using a flat array in first place to improve performance
-//Flatten pieces array for easy shuffling and sampling
-// pieces = pieces[0].concat(pieces[1],pieces[2],pieces[3],pieces[4]);
+/* Game-specific functions */
 
-// //Bind class to collection of pieces
-// pieces = pieces.map(el => new Piece(el.id, el.description, el.syms, el.rots, el.color));
+function gfxSetup(game) {
+  let piecesNode = document.getElementById("availablePieces");
+  piecesNode.innerHTML = game.availablePieces.map((el, i) => ` ${i}. ${el.description}`); //¿Jala?
+  fillGrid(game);
+}
 
+function update(game) {
+  game.timer.tickDown();
+  gfxUpdate(game);
+  if (game.checkGameOver()) {
+    clearInterval(game.interval);
+    gameOver();
+  }
+}
 
-/* Expected flow:
-Set pieces
-Set board (random)
-Set players
-1P:
-  *Set timer
-  *Arrange pieces randomly
-  *P1 selects piece from canvas
-  *P1 flips piece in submenu
-  *P1 selects slot in board
-  *Player checks if piece fits in board
-*/
+function fillGrid(game) {
+  let boardNode = document.getElementById("board");
+  let cells = game.board.slots.length;
+  let rowLength = Math.sqrt(cells);
+  let cellNode;
+  //Iterate over cols
+  for (let j = 0; j < boardNode.children.length; j++) {
+    //Iterate over cells
+    for (let i = 0; i < rowLength; i++) {
+      cellNode = document.createElement("div");
+      cellNode.innerHTML = `${j*rowLength+i}. ${game.board.slots[j*rowLength+i].description}`;
+      boardNode.children[j].appendChild(cellNode);
+    }
+  }
+}
+
+function gfxUpdate(game) {
+  let graphTimer = document.getElementById("testTimer");
+  graphTimer.innerHTML = game.timer.secondsLeft; //¿O usar textNode?
+}
+
+//Called in Game.beginGame to fill players array
+function createPlayer(playerNumber) {
+  let nameTmp = `Which is player #${playerNumber}'s name?`;
+  let colorTmp = `Which is player #${playerNumber}'s color? (Hex: #RRGGBB)`;
+  let name = prompt(nameTmp, `Player ${playerNumber}`);
+  let color = prompt(colorTmp, (playerNumber === 1) ? "#FFC300" : "#FF0000");
+  return new Player(name, color, playerNumber);
+}
+
+function selectedPiece(piece) {
+  let notifNode = document.getElementById("testNotif");
+  notifNode.innerHTML = `You selected ${piece.description},
+                         it's currently in position ${piece.position}`; //¿O usar textNode?
+}
+
+function placedPiece(piece, game) {
+  let piecesNode = document.getElementById("availablePieces");
+  let notifNode = document.getElementById("testNotif");
+  let board = document.getElementById("board");
+  let slot = game.board.slots.indexOf(piece);
+  let rowLength = Math.sqrt(game.board.slots.length);
+  let col = Math.floor(slot/rowLength);
+  let row = slot % rowLength;
+  notifNode.innerHTML = `You placed ${piece.description},
+                         it's currently in board slot ${game.board.slots.indexOf(piece)}`; //¿O usar textNode?
+  piecesNode.innerHTML = game.availablePieces.map((el, i) => ` ${i}. ${el.description}`); //¿Jala?
+  //This strikes through the piece and sets font to gray
+  board.children[col].children[row].setAttribute("class", "inSlot"); //Refer to test.css
+}
+
+//TODO: Upgrade this
+function wrongPiece() {
+  let notifNode = document.getElementById("testNotif");
+  notifNode.innerHTML = "Wrong piece!!"; //¿O usar textNode?
+}
+
+//TODO: Upgrade this
+function wrongRot() {
+  let notifNode = document.getElementById("testNotif");
+  notifNode.innerHTML = "It didn't quite fit in..."; //¿O usar textNode?
+}
+
+//TODO: Build this
+function wrongTone() {
+  //Play audio with wrong tone
+}
+
+//TODO: Build this
+function updateHighlights() {
+
+}
+
+function gameOver() {
+  alert("¡Destreza!"); //TODO: Sustituir por animación chida
+}
--- js/game.js
+++ js/game.js
@@ -1,5 +1,18 @@
+/* Expected flow:
+Set pieces
+Set board (random)
+Set players
+1P:
+  *Set timer
+  *Arrange pieces randomly
+  *P1 selects piece from canvas
+  *P1 flips piece in submenu
+  *P1 selects slot in board
+  *Player checks if piece fits in board
+*/
 
 /* Set up game*/
+
 //Consider outright using a flat array in first place to improve performance
 //Flatten pieces array for easy shuffling and sampling
 pieces = pieces[0].concat(pieces[1],pieces[2],pieces[3],pieces[4]);
@@ -9,11 +22,14 @@ pieces = pieces.map(el => new Piece(el.id, el.description, el.syms, el.rots, el.
 
 //Test mode:
 //Generate 3x3 board
+// testGame = new Game("Solo", pieces);
+
+// testBoard = new Board(3);
+// testBoard.fillBoard();
+// testGame.filterPieces(testBoard);
+
+// //Players
+// tester = new Player("Test", "#FF0000", 1);
+
 testGame = new Game("Solo", pieces);
-
-testBoard = new Board(3);
-testBoard.fillBoard();
-testGame.filterPieces(testBoard);
-
-//Players
-tester = new Player("Test", "#FF0000", 1);
+testGame.beginGame();
